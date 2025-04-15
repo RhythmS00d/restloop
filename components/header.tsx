a378d5dd6287dba42754422e0522e53ba170e4dc
RhythmS00d <rhythmsood1@gmail.com>
2025-04-16 09:19:09 +1000

    added shadcn components

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import { Separator } from "@/components/ui/separator";
+
+export default function Header() {
+    return (
+        <header>
+            <h2 className="text-5xl">RESTLOOP</h2>
+            <Separator/>
+        </header>
+    )
+}
